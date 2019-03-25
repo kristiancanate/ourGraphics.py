63615f92ac4c51518b4dc7d0136f2b27fee8069c
csPython <cspython@mbhscart6-23.mbhs.lab>
2019-03-25 10:54:13 -0400

    adds graphic window

---
+++ ourGraphics.py
@@ -0,0 +1,13 @@
+#ourGraphics by Mariana and Kristian
+from graphics import *
+
+#Initializing variables
+winSz = 900
+
+#Defining Window
+NPwin = GraphWin("North Pole", winSz, winSz)
+NPwin.setCoords(0, 0, winSz, winSz)
+
+#Closes window with user click
+NPwin.getMouse()
+NPwin.close()
