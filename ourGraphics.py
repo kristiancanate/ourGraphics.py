4aa6f37ccdadf152d6bc6c968c3d92717d2b5a2c
csPython <cspython@mbhscart6-23.mbhs.lab>
2019-03-25 10:57:11 -0400

    adds sky background

--- ourGraphics.py
+++ ourGraphics.py
@@ -8,6 +8,11 @@ winSz = 900
 NPwin = GraphWin("North Pole", winSz, winSz)
 NPwin.setCoords(0, 0, winSz, winSz)
 
+#Drawing sky background
+sky = Rectangle(Point(0, 0), Point(winSz, winSz))
+sky.setFill(color_rgb(50, 100, 150))
+sky.draw(NPwin)
+
 #Closes window with user click
 NPwin.getMouse()
 NPwin.close()
